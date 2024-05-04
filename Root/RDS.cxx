e911c5c9f6a2bad313700b1ddb5873b2e675b682
taehyounpark <taehyounpark@icloud.com>
2024-05-04 12:09:53 -0400

    Check ROOT version for RDS

--- Root/RDS.cxx
+++ Root/RDS.cxx
@@ -13,10 +13,16 @@ AnaQ::RDS::partition() {
   return m_rds->GetEntryRanges();
 }
 
-void AnaQ::RDS::initialize() { m_rds->Initialise(); }
+void AnaQ::RDS::initialize() {
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6, 28, 00)
+  m_rds->Initialize();
+#else
+  m_rds->Initialise();
+#endif
+}
 
 void AnaQ::RDS::initialize(unsigned int slot, unsigned long long begin,
-                     unsigned long long /* end */) {
+                           unsigned long long /* end */) {
   m_rds->InitSlot(slot, begin);
 }
 
@@ -24,6 +30,18 @@ void AnaQ::RDS::execute(unsigned int slot, unsigned long long entry) {
   m_rds->SetEntry(slot, entry);
 }
 
-void AnaQ::RDS::finalize(unsigned int slot) { m_rds->FinaliseSlot(slot); }
+void AnaQ::RDS::finalize(unsigned int slot) {
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6, 28, 00)
+  m_rds->FinalizeSlot(slot);
+#else
+  m_rds->FinaliseSlot(slot);
+#endif
+}
 
-void AnaQ::RDS::finalize() { m_rds->Finalise(); }
+void AnaQ::RDS::finalize() {
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6, 28, 00)
+  m_rds->Finalize();
+#else
+  m_rds->Finalise();
+#endif
+}
