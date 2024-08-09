6b2984a2ec2e2f61df14ce7c0d3e351b1da7837a
Tae Hyoun Park <tae.hyoun.park@cern.ch>
2024-08-09 11:34:31 +0200

    Tree data

--- AnalysisQuery/TreeData.h
+++ AnalysisQuery/TreeData.h
@@ -24,8 +24,6 @@ public:
 
   template <typename T> class Branch;
 
-  template <typename... ColumnTypes> class Snapshot;
-
 public:
   TreeData(const std::vector<std::string> &filePaths, const std::string &treeName);
   TreeData(std::initializer_list<std::string> filePaths,
@@ -140,93 +138,8 @@ protected:
   mutable ROOT::RVec<bool> m_readArray;
 };
 
-template <typename... ColumnTypes>
-class TreeData::Snapshot
-    : public queryosity::query::definition<std::shared_ptr<TTree>(
-          ColumnTypes...)> {
-
-public:
-  static constexpr size_t N = sizeof...(ColumnTypes);
-  using BranchArray_t = std::array<TBranch *, N>;
-  using ColumnTuple_t = std::tuple<ColumnTypes...>;
-
-public:
-  template <typename... Names>
-  Snapshot(const std::string &treeName, Names const &...columnNames);
-  virtual ~Snapshot() = default;
-
-  virtual void fill(queryosity::column::observable<ColumnTypes>...,
-                    double) final override;
-  virtual std::shared_ptr<TTree> result() const final override;
-  virtual std::shared_ptr<TTree>
-  merge(std::vector<std::shared_ptr<TTree>> const &results) const final override;
-
-private:
-  // helper function to make branch of i-th data type with i-th column name
-  template <typename T, std::size_t I>
-  TBranch *makeBranch(const std::string &name) {
-    return m_snapshot->Branch<T>(name.c_str(), &std::get<I>(m_columns));
-  }
-
-  template <std::size_t I, typename T>
-  void fillBranch(const queryosity::column::observable<T> &column) {
-    std::get<I>(m_columns) = column.value();
-  }
-
-  // expand the packs ColumnTypes, Indices, and columnNames simultaneously
-  template <std::size_t... Is, typename... Names>
-  BranchArray_t makeBranches(std::index_sequence<Is...>,
-                             Names const &...columnNames) {
-    return {{makeBranch<ColumnTypes, Is>(columnNames)...}};
-  }
-
-  template <std::size_t... Is, typename... Observables>
-  void fillBranchs(std::index_sequence<Is...>, Observables... columns) {
-    (fillBranch<Is>(columns), ...);
-  }
-
-protected:
-  std::shared_ptr<TTree> m_snapshot; //!
-  BranchArray_t m_branches;
-  ColumnTuple_t m_columns;
-};
-
 template <typename U>
 std::unique_ptr<TreeData::Branch<U>> TreeData::read(unsigned int slot,
                                             const std::string &branchName) {
   return std::make_unique<Branch<U>>(branchName, *m_treeReaders[slot]);
-}
-
-template <typename... ColumnTypes>
-template <typename... Names>
-TreeData::Snapshot<ColumnTypes...>::Snapshot(const std::string &treeName,
-                                         Names const &...columnNames)
-    : m_snapshot(std::make_shared<TTree>(treeName.c_str(), treeName.c_str())),
-      m_branches(makeBranches(std::index_sequence_for<ColumnTypes...>(),
-                              columnNames...)) {
-  m_snapshot->SetDirectory(0);
-}
-
-template <typename... ColumnTypes>
-void TreeData::Snapshot<ColumnTypes...>::fill(
-    queryosity::column::observable<ColumnTypes>... columns, double) {
-  this->fillBranchs(std::index_sequence_for<ColumnTypes...>(), columns...);
-  m_snapshot->Fill();
-}
-
-template <typename... ColumnTypes>
-std::shared_ptr<TTree> TreeData::Snapshot<ColumnTypes...>::result() const {
-  return m_snapshot;
-}
-
-template <typename... ColumnTypes>
-std::shared_ptr<TTree> TreeData::Snapshot<ColumnTypes...>::merge(
-    std::vector<std::shared_ptr<TTree>> const &results) const {
-  TList list;
-  for (auto const &result : results) {
-    list.Add(result.get());
-  }
-  auto merged = std::shared_ptr<TTree>(TTree::MergeTrees(&list));
-  merged->SetDirectory(0);
-  return merged;
 }
